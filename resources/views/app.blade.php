728c0d019925a5bf65c9082f21f60ff45be36b71
mo7ammedahmed <mo7ammed99887@gmail.com>
2026-07-28 21:16:56 +0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-
+        <meta name="google-site-verification" content="MlBLjk8L0D-TBSquV-4PtBobRbjuJ1pl1PQVatc-wf4" />
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
             (function() {
